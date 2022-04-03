9b732db2d0ee2f13701f6193d7a75b9c36a74fec
ShobhitGoel102 <itsshobhitgoel@gmail.com>
2022-04-03 11:49:05 +0530

    feat: added show watched time in minutes and emergency navigation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import Navbar from "./components/Navbar/Navbar";
 import Intro from "./components/Intro/Intro";
 import Services from "./components/Services/Services";
 import "./App.css";
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 import Experience from "./components/Experience/Experience";
 import Works from "./components/Works/Works";
 import Portfolio from "./components/Portfolio/Portfolio";
@@ -10,9 +14,28 @@ import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 import { useContext } from "react";
 import { themeContext } from "./Context";
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCjWO9bPrYzUHnneY3nMNY0aL9dpJVGRpc",
+  authDomain: "shobhitdev-782b8.firebaseapp.com",
+  projectId: "shobhitdev-782b8",
+  storageBucket: "shobhitdev-782b8.appspot.com",
+  messagingSenderId: "374767910597",
+  appId: "1:374767910597:web:099d995c5201175f273aac",
+  measurementId: "G-C9E0DT5LES"
+};
+
 function App() {
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
+
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
   return (
     <div
       className="App"
